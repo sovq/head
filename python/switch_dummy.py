3cf367238a6e335b88a6b846d0a928ae682c46e9
GrowManager <blazejkoszny@gmail.com>
2015-01-09 23:08:17 +0100

    dummy devices fix

--- python/switch_dummy.py
+++ python/switch_dummy.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 import sys
 
+argumentPort = sys.argv[1]
+portNumber = int(argumentPort)
+argumentState = sys.argv[2]
+
 if(argumentState=="on"):
 	print "on"
 elif(argumentState=="off"):
